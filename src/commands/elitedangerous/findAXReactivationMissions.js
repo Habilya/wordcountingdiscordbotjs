03f9d27f487ac0f1f8a418e91e7045dfa8245deb
Habilya <ciuvilin.co@gmail.com>
2023-10-29 19:06:29 -0400

    adjusted findAXReactivationMissions command

--- src/commands/elitedangerous/findAXReactivationMissions.js
+++ src/commands/elitedangerous/findAXReactivationMissions.js
@@ -1,7 +1,8 @@
 const { ApplicationCommandOptionType, PermissionFlagsBits } = require("discord.js");
+const { AsciiTable3, AlignmentEnum } = require('ascii-table3');
+
 const apiGetDcohWatchAPIOverview = require('../../utils/apiGetDcohWatchAPIOverview');
 const apiGetInaraMilitarySettlements = require('../../utils/apiGetInaraMilitarySettlements');
-const { AsciiTable3, AlignmentEnum } = require('ascii-table3');
 
 module.exports = {
     name: "findaxreactivationmissions",
@@ -16,36 +17,37 @@ module.exports = {
             // have to use deferred reply, because the processing time is long...
             await interaction.deferReply();
 
-            apiGetDcohWatchAPIOverview(async function (overview) {
-                const filteredSystems = overview.systems.filter(x =>
-                    x.features.includes("ThargoidControlledReactivationMissions") &&
-                    (x.thargoidLevel.level == 20 || x.thargoidLevel.level == 30)
-                );
+            const overview = await apiGetDcohWatchAPIOverview();
 
-                var table = new AsciiTable3('AX Reactivation missions')
-                    .setHeading('System Name', 'Thargoid State', 'Titan', 'M. settlements')
-                    .setAlign(4, AlignmentEnum.CENTER)
-                    .setStyle('unicode-double');
+            const filteredSystems = overview.systems.filter(x =>
+                x.features.includes("ThargoidControlledReactivationMissions") &&
+                (x.thargoidLevel.level == 20 || x.thargoidLevel.level == 30)
+            );
 
-                for(const system of filteredSystems) {
+            var table = new AsciiTable3('AX Reactivation missions')
+                .setHeading('System Name', 'Thargoid State', 'Titan', 'M. settlements')
+                .setAlign(4, AlignmentEnum.CENTER)
+                .setStyle('unicode-double');
 
-                    let militarySettlementsCount = 0;
-                    
-                    try {
-                        const systemsData = await apiGetInaraMilitarySettlements(system.name);
-                        const filteredMatchingSettlements = systemsData.filter(x => parseFloat(x.Dist) < 20);
-                        militarySettlementsCount = filteredMatchingSettlements.length
-                    } catch(error) {
-                        discordBot.getLogger().error(`Unhandled exception while getting nb military settleemnts: ${error}\n${error.stack}`);
-                    }
+            for(const system of filteredSystems) {
 
-                    table.addRow(system.name, system.thargoidLevel.name, system.maelstrom.name, militarySettlementsCount);
+                let militarySettlementsCount = 0;
+
+                try {
+                    const systemsData = await apiGetInaraMilitarySettlements(system.name);
+                    const filteredMatchingSettlements = systemsData.filter(x => parseFloat(x.Dist) < 20);
+                    militarySettlementsCount = filteredMatchingSettlements.length
+                } catch(error) {
+                    discordBot.getLogger().error(`Unhandled exception while getting nb military settleemnts: ${error}\n${error.stack}`);
                 }
 
-                interaction.editReply({
-                    content: '```' + table.toString() + '```',
-                });
+                table.addRow(system.name, system.thargoidLevel.name, system.maelstrom.name, militarySettlementsCount);
+            }
+
+            interaction.editReply({
+                content: '```' + table.toString() + '```',
             });
+
         } catch(error) {
             discordBot.getLogger().error(`Unhandled exception (find AX Reactivation Missions) while calling API: ${error}\n${error.stack}`);
         }
--- src/utils/apiGetDcohWatchAPIOverview.js
+++ src/utils/apiGetDcohWatchAPIOverview.js
@@ -1,18 +1,7 @@
-const https = require('https');
+const axios = require('axios');
 
-module.exports = (apiResponseJSONDataCallBack) => {
-    https.get(`https://dcoh.watch/api/v1/Overwatch/OverviewV2`, resp => {
-        let data = "";
-
-        // A chunk of data has been recieved.
-        resp.on("data", chunk => {
-            data += chunk;
-        });
-
-        // The whole response has been received.
-        resp.on("end", () => {
-            const overview = JSON.parse(data);
-            apiResponseJSONDataCallBack(overview);
-        });
-    });
+module.exports = async() => {
+    const response = await axios.get(`https://dcoh.watch/api/v1/Overwatch/OverviewV2`);
+    
+    return response.data;
 };
